376e3b26c44f29900bdba2ae986eac763c7510ee
DineshRinwa <dineshrinwa410@gmail.com>
2024-02-24 12:07:57 +0530

    Introduction to Javascript Problems are pushing

---
+++ Introduction_to_Javascript.js
@@ -0,0 +1,21 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Dinesh";
+console.log(name);
+
+let father_name = "Tejaram";
+console.log(father_name);
+
+let mother_name = "Santosh";
+console.log(mother_name)
+
+
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School");
+console.log("A Transformation in Education");
